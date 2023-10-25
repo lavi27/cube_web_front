c9b9ad4c378fda5a4f88cb03ccc979324d5c6f05
lavi <62757958+lavi27@users.noreply.github.com>
2023-10-26 02:44:29 +0900

    a

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "@styles/pages/home.module.scss"
 import { getSearch } from "@app/_api";
 import { Post as PostType } from "@app/_types";
@@ -7,62 +7,70 @@ import Post from "@components/post";
 
 export default function Comp() {
   const [posts, setPosts] = useState<PostType[]>();
-
   const [scrollTop, setScrollTop] = useState(0);
   const [mouseDownY, setMouseDownY] = useState(0);
   const [mousePullY, setMousePullY] = useState(0);
-
   const [firstPostTimestamp, setFirstPostTimestamp] = useState(0);
   const [lastPostTimestamp, setLastPosTimestamp] = useState(0);
-
   const postWrapRef = useRef<HTMLDivElement>(null);
   const isLoaded = posts !== undefined;
 
-  const refreshContents = () => {
-    getSearch({})
-      .then((data) => {
-        if (!posts) return;
+  const mouseDown = useCallback((e: MouseEvent) => {
+    setMouseDownY(e.clientY);
+  }, [])
+  const mouseMove = useCallback((e: MouseEvent) => {
+    setMousePullY(e.clientY - mouseDownY);
+  }, [mouseDownY])
+  const mouseUp = useCallback(() => {
+    const refreshContents = () => {
+      getSearch({})
+        .then((data) => {
+          if (!posts) return;
 
-        let tmp = [...data];
-        const firstPostIndex = tmp.findIndex((post) => post.createDate == firstPostTimestamp);
-        if (firstPostIndex != -1) {
-          tmp = tmp.slice(0, firstPostIndex)
-        }
+          let tmp = [...data];
+          const firstPostIndex = tmp.findIndex((post) => post.createDate == firstPostTimestamp);
+          if (firstPostIndex != -1) {
+            tmp = tmp.slice(0, firstPostIndex)
+          }
 
-        setPosts([...tmp, ...posts]);
-        setFirstPostTimestamp(data[0].createDate)
-      })
-  }
+          setPosts([...tmp, ...posts]);
+          setFirstPostTimestamp(data[0].createDate)
+        })
+    }
 
-  const loadNextContents = () => {
-    getSearch({dateFrom: lastPostTimestamp - 1})
-      .then((data) => {
-        if (!posts) return;
+    const loadNextContents = () => {
+      getSearch({ dateFrom: lastPostTimestamp - 1 })
+        .then((data) => {
+          if (!posts) return;
 
-        setPosts([...posts, ...data]);
-        setLastPosTimestamp(data[data.length - 1].createDate)
-      })
-  }
+          setPosts([...posts, ...data]);
+          setLastPosTimestamp(data[data.length - 1].createDate)
+        })
+    }
+
+    console.log(mouseDownY);
+    console.log(mousePullY);
+
+    if (Math.abs(mousePullY) <= 30) return;
+
+    if (scrollTop == 0) {
+      refreshContents()
+    } else if (scrollTop == postWrapRef.current?.clientHeight) {
+      loadNextContents()
+    }
+  }, [firstPostTimestamp, lastPostTimestamp, mouseDownY, mousePullY, posts, scrollTop])
 
   useEffect(() => {
-    document.addEventListener("mousedown", (e) => {
-      setMouseDownY(e.pageY);
-    })
+    document.addEventListener("mousedown", mouseDown)
+  }, [mouseDown])
+  useEffect(() => {
+    document.addEventListener("mousemove", mouseMove)
+  }, [mouseMove])
+  useEffect(() => {
+    document.addEventListener("mouseup", mouseUp)
+  }, [mouseUp])
 
-    document.addEventListener("mousemove", (e) => {
-      setMousePullY(e.pageY - mouseDownY);
-    })
-
-    document.addEventListener("mouseup", (e) => {
-      if (Math.abs(mousePullY) <= 30) return;
-
-      if (scrollTop == 0) {
-        refreshContents()
-      } else if (scrollTop == postWrapRef.current?.clientHeight) {
-        loadNextContents()
-      }
-    })
-
+  useEffect(() => {
     getSearch({})
       .then(data => {
         setPosts(data);
@@ -87,7 +95,7 @@ export default function Comp() {
   return (
     <div
       className={styles.home_wrap}
-      onScroll={(e) => {setScrollTop(e.currentTarget.scrollTop)}}
+      onScroll={(e) => { setScrollTop(e.currentTarget.scrollTop) }}
       ref={postWrapRef}
     >
       {
